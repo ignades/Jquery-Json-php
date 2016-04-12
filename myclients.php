a05dec491d8dfbb2b836b89f5e3389e1c4102ce1
ignades <ignades@users.noreply.github.com>
2016-04-12 09:54:11 -0300

    Update myclients.php
    act clients

--- myclients.php
+++ myclients.php
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-<?php
-
-$connection = mysql_connect("localhost", "root", "password"); // Establishing Connection with Server..
-$db = mysql_select_db("formulario", $connection); // Selecting Database
-//etraggo le singole categories`
-$sqlcat="SELECT * FROM formulario.clientes
-inner join formulario.articulos on formulario.articulos.id = formulario.clientes.id 
-;";
-$result=mysql_query($sqlcat);
  
-$todos=array();
-
-while ($row = mysql_fetch_array($result)) 
-{
-
-   // $row_array['id'] = $row['id'];
-    $array_01["justIn"][]['nombre']= $row['nombre'];
-   // $row_array['apellido'] = $row['apellido'];
-  //  $row_array['nc'] = $row['nc'];
-    $array_02["Articulos"][]['name_article'] = $row['txt'];
-
-
-$todos = array($array_01,$array_02);
-  
-}
-
-
-$res =  json_encode($todos);
-echo $res;
-
-?>
-
-=======
 <?php
 
 /*
@@ -52,7 +19,7 @@ $res = json_encode($arraynames);
 
 echo $res;
 */
-$connection = mysql_connect("localhost", "root", "Ignazio01!"); // Establishing Connection with Server..
+$connection = mysql_connect("localhost", "root", "Ignazio"); // Establishing Connection with Server..
 $db = mysql_select_db("formulario", $connection); // Selecting Database
 //etraggo le singole categories`
 $sqlcat="SELECT * FROM formulario.clientes
@@ -82,4 +49,4 @@ echo $res;
 
 ?>
 
->>>>>>> f9428754c9311b9767869e890ede730800c9237f
+ 
